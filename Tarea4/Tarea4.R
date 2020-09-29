c09715f961acd5c9abe87b306d693b5c8d57f45c
JohannaBZ <jbz1907@hotmail.com>
2020-09-29 10:37:01 -0500

    Tarea4

--- Tarea4/Tarea4.R
+++ Tarea4/Tarea4.R
@@ -1,28 +1,28 @@
 # Modelos Probabilistas Aplicados
-# Tarea 1
-# Tema: Analisis de texto - Distribuciones
+# Tarea 4
+# Tema: Distribucion de Poisson --- aproximaciones
 # Johanna Bolaños Zuñiga
 
 # Parametros
 meta = 1                  
 lambda = 2			
-mu = exp(-la)		
+mu = exp(-lambda)		
 cuni = numeric()
 cexp = numeric()
-repl = 1000	
+n = 1000	#replicas
 
 
 #Exponencial
-for (replica in 1:repl) {
+for (replica in 1:n) {
     dexp = numeric()
     while (sum(dexp) < meta) {
-        dexp = c(dexp, rexp(1, la))
+        dexp = c(dexp, rexp(1, lambda))
     }
     cexp = c(cexp, length(dexp))
 }
 
 #Uniforme
-for (replica in 1:repl) {
+for (replica in 1:n) {
     duni = c(1)
     while (prod(duni) > mu) {
         duni = c(duni, runif(1))
@@ -40,9 +40,10 @@ colpoi <- c("red")
 labelMexp <- c("Dist Exponencial con n=100")
 labelMuni <- c("Dist Uniforme con n=100")
 labelMpoi <- c("Dist Poisson con n=100")
-png("1000n-la2.png", width=5000,height=3000,res=1000)
+
+png("1000n-la2.png", width=8000,height=3000,res=1000)
 par(mfcol = c(1, 3))
 hist(cuni, col=coluni, main=labelMuni, xlab=labelX, ylab=labelY)
 hist(cexp, col=colexp, main=labelMexp, xlab=labelX, ylab=labelY)
-hist(rpois(repl, la), col=colpoi, main=labelMpoi, xlab=labelX, ylab=labelY)
+hist(rpois(n, lambda), col=colpoi, main=labelMpoi, xlab=labelX, ylab=labelY)
 dev.off()
